6743d794f9fae5c1418980ce8474e999bea61450
leejeongsoo1660 <74721370+leejeongsoo1660@users.noreply.github.com>
2022-03-31 14:09:43 +0900

    board models
    boa]r

--- board/models.py
+++ board/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from developer.models import Developer
 
 
-class Board(models.Modle):
+class Board(models.Model):
     developer = models.ForeignKey(Developer, on_delete=models.CASCADE, verbose_name='개발자')
     language = models.IntegerField(default=0, verbose_name='언어')
     title = models.CharField(max_length=20, verbose_name='제목')
@@ -17,9 +17,9 @@ class Board(models.Modle):
         verbose_name_plural = '게시판(들)'
 
 class Boardimg(models.Model):
-    board = models.ForeignKey(Board, on_delete=models.CASCADE, primary_key=True, verbose_name='게시판')
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, verbose_name='게시판')
     boardimg = models.TextField(verbose_name='게시판그림')
-    img_original = models.CharField(null=False)
+    img_original = models.CharField(max_length=200, null=False)
 
     class Meta:
         db_table = 'opd_boardimg'
