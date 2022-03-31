7ac09b3f0075039997cc2eb9bf6f1720677ac9ef
CDGDG <whehdrjs207@naver.com>
2022-03-31 12:08:26 +0900

    media setting 공유
    media 폴더는 .gitignore에 포함되서
    알아서 만들어야함
    
    OPD_Project 밑에 media 폴더 생성 후
    거기에 테이블이름_사진컬럼이름 으로
    예) project_thumbnail
    boardimg_img

--- project/models.py
+++ project/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=50, verbose_name='프로젝트 타이틀')
+    summary = models.CharField(max_length=100, verbose_name='프로젝트 요약')
+    contents = models.TextField(verbose_name='프로젝트 내용')
+    startdate = models.DateTimeField()
+    enddate = models.DateTimeField()
+    viewcnt = models.IntegerField(default=0)
+    private = models.BooleanField(default=False)
+    # thumbnail = models.
