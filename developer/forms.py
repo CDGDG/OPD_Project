bbf7e917a190acf9a08529a8822f8431922b50d4
hyewwon <rlagp424@gmail.com>
2022-04-01 13:59:03 +0900

    내꺼

--- developer/forms.py
+++ developer/forms.py
@@ -1,8 +1,9 @@
-from cProfile import label
-from secrets import choice
 from django import forms
+
+import developer
 from .models import Developer
 from project.models import Language
+from django.contrib.auth.hashers import make_password
 
 class JoinForm(forms.Form):
     userid = forms.CharField(
@@ -11,7 +12,7 @@ class JoinForm(forms.Form):
         },
         max_length=20,label='아이디'
     )
-    passwored = forms.CharField(
+    password = forms.CharField(
         error_messages={
             'required' : '비밀번호를 입력해주세요'
         },
@@ -47,29 +48,46 @@ class JoinForm(forms.Form):
         },
         max_length=15,label='이메일'
     )
-
     pic = forms.FileField(label='프로필사진')
-
     resume = forms.FileField(label='이력서')
-
     CHOICES = []
     for lang in Language.objects.all():
         CHOICES.append((lang.pk,lang.language))
-    
 
     language = forms.MultipleChoiceField(label="언어 선택",widget=forms.CheckboxSelectMultiple, choices=CHOICES)
 
   
     def clean(self):
         cleaned_data = super().clean()
+        self.userid = cleaned_data.get('userid')
+        self.password = cleaned_data.get('password')
+        self.check_password = cleaned_data.get('check_password')
+        self.nickname = cleaned_data.get('nickname')
+        self.registnum = cleaned_data.get('registnum')
+        self.phonenum = cleaned_data.get('phonenum')
+        self.email = cleaned_data.get('email')
+        self.language = cleaned_data.get('language')
 
-        userid = cleaned_data.get('userid')
-        password = cleaned_data.get('password')
-        check_password = cleaned_data.get('check_password')
-        nickname = cleaned_data.get('nickname')
-        registnum = cleaned_data.get('registnum')
-        phonenum = cleaned_data.get('phonenum')
-        email = cleaned_data.get('email')
+    
+        # developer = Developer(
+        #             userid = cleaned_data.get('userid'),
+        #             password = cleaned_data.get('password'),
+        #             nickname = cleaned_data.get('nickname'),
+        #             registnum = cleaned_data.get('registnum'),
+        #             phonenum = cleaned_data.get('phonenum'),
+        #             email = cleaned_data.get('email'),
+        #             language = cleaned_data.get('language'),
+        #             resume = cleaned_data.get('resume'),
+        #             pic = cleaned_data.get('pic'),
+        # )
+        # print(developer)
+
+        # developer.save()
+    
+
+
+
+
 
 
 
--- developer/views.py
+++ developer/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from .models import Developer
 from .forms import JoinForm
+from django.contrib.auth.hashers import make_password
+# check_password
 
 def join(request):
     if request.method=="POST":
-        form = JoinForm(request.POST)
+        form = JoinForm(request.POST,request.FILES)
+        if form.is_valid():
+            developer = Developer(
+                    userid = form.userid,
+                    password = make_password(form.password),
+                    nickname = form.nickname,
+                    registnum = form.registnum,
+                    phonenum = form.phonenum,
+                    email = form.email,
+                    language = form.language,
+                    resume = request.FILES['resume'],
+                    # resume_original = form['resume'].name,
+                    pic = request.FILES['pic'],
+                    # pic_original = form['pic'].name,
+            )
+            developer.save()
+
+            return render(request,'developer_join.html')
     else:
         form = JoinForm()
     return render(request,'developer_join.html',{'form':form})
